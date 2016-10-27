5e77d60396d2ada4877d982c672b82f373522731
hhm <hehongmeizengxi@163.com>
2016-10-28 00:48:21 +0800

    fixed the bug in multiselection and clean selection button

--- public/js/upload/info.js
+++ public/js/upload/info.js
@@ -1,9 +1,10 @@
 /**
  * Created by heli on 2016/10/25 0025.
  */
+'use strict';
 const INFO_THEADER = [
   {title: '商品ID'},
-  {title: '入库时间'},
+  {title: '入库时间', width: '5%'},
   {title: '主类'},
   {title: '子类1'},
   {title: '子类2'},
@@ -13,15 +14,17 @@ const INFO_THEADER = [
   {title: '券后价格'},
   {title: '优惠价格'},
   {title: '收藏数'},
-  {title: '图片1'},
-  {title: '图片2'},
-  {title: '图片3'},
+  {title: '图片1', swidth: '5%'},
+  {title: '图片2', swidth: '5%'},
+  {title: '图片3', swidth: '5%'},
 ];
+
 
 $(function () {
   // 信息以二维数据存放
   // ajax从服务器获取商品数据列表
-  var bDelRowActive = false;
+  let bDelRowActive = false;
+  let selNum        = 0;    // 标记当前选择行的数量
   const listInfo    = function () {
     $.ajax({
       url     : '/info/listgoods',
@@ -37,23 +40,23 @@ $(function () {
           data.push(row);
         });
         $('#table-goods-list').DataTable({
+          autoWidth : false,
           destroy   : true,
           lengthMenu: [[20, 50, 100, -1], [20, 50, 100, "All"]],
           data      : data,
           columns   : INFO_THEADER
         });
         // 监听多选
-        let selNum = 0;    // 标记从0->1还是从1->0
         $('#table-goods-list tbody').on('click', 'tr', function () {
           if (bDelRowActive) {
             $(this).toggleClass('selected');
-            // 改变删除数据按钮颜色与提示内容
-            if($('.selected').length==1 && selNum==0){
+            // 改变删除数据提示内容
+            if ($('.selected').length == 1 && selNum == 0) {
               selNum = 1;
-              $('#btn-delRow').toggleClass('btn-info-actived').html('删除所选')
-            }else if($('.selected').length==0 && selNum==1){
+              $('#btn-delRow').html('删除所选')
+            } else if ($('.selected').length == 0 && selNum == 1) {
               selNum = 0;
-              $('#btn-delRow').toggleClass('btn-info-actived').html('多选');
+              $('#btn-delRow').html('多选');
             }
           }
         });
@@ -79,22 +82,29 @@ $(function () {
   $('#btn-delRow').click(function () {
     // 如果关闭删除，则清掉之前的所有多选项
     if (bDelRowActive === true) {
-      $('#btn-clearSel').button('disable');
-      $('.selected').removeClass('selected');
-      // 清除按钮选择状态
-      $('#btn-delRow').removeClass('btn-info-actived');
-      $('#btn-delRow').html('多选');
-    }else{
+      // 如果当前已经选择了项,则进行删除处理
+      if (selNum === 1) {
+
+      } else {  // 否则仅仅关闭多选
+        $('#btn-clearSel').button('disable');
+        $('.selected').removeClass('selected');
+        // 清除按钮选择状态
+        $('#btn-delRow').removeClass('btn-info-actived');
+        $('#btn-delRow').html('多选');
+        bDelRowActive = !bDelRowActive;
+      }
+    } else {
       $('#btn-delRow').addClass('btn-info-actived').blur();
       $('#btn-clearSel').button('enable');
+      bDelRowActive = !bDelRowActive;
     }
-    bDelRowActive = !bDelRowActive;
   });
 
   $('#btn-clearSel').click(function () {
     if (bDelRowActive === true) {
       $(this).button('disable');
       $('.selected').removeClass('selected');
+      selNum = 0;
       // 清除按钮选择状态
       $('#btn-delRow').removeClass('btn-info-actived');
       $('#btn-delRow').html('多选');
