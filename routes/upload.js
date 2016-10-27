26ff9d7f59643db0b6fb0311c392e0bd7836ee8b
hehongmeizengxi@163.com <hehongmeizengxi@163.com>
2016-10-27 18:49:22 +0800

    store pictures which is initializing with a unique name to public/images and store pictures's routin to mysql instead of DataUrl

--- routes/upload.js
+++ routes/upload.js
@@ -3,11 +3,13 @@
  */
 const express       = require('express');
 const router        = express.Router();
+const fs            = require("fs");
 const path          = require('path');
 const selectContent = require('../config/selectConfig.js')
 const BBPromise     = require('bluebird');
 const multiparty    = require('multiparty');
 const dbStore       = require('../db/index');
+const config          = require('../config');
 
 /* GET users listing. */
 router.get('/', function (req, res) {
@@ -36,6 +38,14 @@ router.post('/', function (req, res) {
     const pic2      = objs.file1 === undefined ? null : objs.file1[0];
     const pic3      = objs.file2 === undefined ? null : objs.file2[0];
 
+    // 保存图片到本地images/goodspic/目录下 返回路径
+    let storePics = [];
+    if(pic1!==null) storePics.push(pic1);
+    if(pic2!==null) storePics.push(pic2);
+    if(pic3!==null) storePics.push(pic3);
+
+    const picsContents = storeGoodsPics(storePics);
+
     const tblName       = 'tbl_store';
     const sqlInsertData = `INSERT INTO ${tblName} VALUES (
         0, NOW(), $type, $subtype1, $subtype2, $title, $step1, $step2, $realPrice,
@@ -53,9 +63,9 @@ router.post('/', function (req, res) {
           realPrice: realPrice,
           coupon   : coupon,
           collect  : collect,
-          pic1     : pic1,
-          pic2     : pic2,
-          pic3     : pic3
+          pic1     : picsContents[0]===undefined? null:picsContents[0],
+          pic2     : picsContents[1]===undefined? null:picsContents[1],
+          pic3     : picsContents[2]===undefined? null:picsContents[2]
         }
       })
     ]).spread(function (data) {
@@ -68,5 +78,37 @@ router.post('/', function (req, res) {
   });
 
 });
+
+/**
+ * 根据服务器ID(4位) + 时间戳(14位) + 自增数字产生UID数字
+ */
+let uid_incr = 0;
+function generateUIDName() {
+  return config.svrId.toString() + new Date().getTime().toString() + (++uid_incr);
+}
+
+/**
+ * 保存图片到本地images/goodspic/目录下
+ * 返回存储路径（包括新定义的随机名字）
+ * @param pics
+ */
+function storeGoodsPics(pics) {
+  let rePicsContent = [];
+  pics.forEach(function (picBase64) {
+    picBase64      = picBase64.replace(/^data:image\/\w+;base64,/, "");
+    let dataBuffer = new Buffer(picBase64, 'base64');
+    let picName    = generateUIDName();
+    rePicsContent.push(`/images/goodspic/${picName}.jpg`);
+    console.log(picName);
+    fs.writeFile(`public/images/goodspic/${picName}.jpg`, dataBuffer, function (err) {
+      if(err){
+        throw `ERROR: Store picture dataUrl as jpg failed! Check the formation of picture is .jpg`;
+      }else{
+        console.log(`stored as file ${picName}.jpg`)
+      }
+    })
+  });
+  return rePicsContent;
+}
 
 module.exports = router;
