18730f1ccc32e43caf12cb8b94185b844f915aa6
hehongmeizengxi@163.com <hehongmeizengxi@163.com>
2016-11-05 12:25:01 +0800

    mem leak: file delete when good deleted

--- routes/info.js
+++ routes/info.js
@@ -4,6 +4,7 @@
 const express   = require('express');
 const router    = express.Router();
 const path      = require('path');
+const fs        = require('fs');
 const BBPromise = require('bluebird');
 const dbStore   = require('../db/index');
 
@@ -25,14 +26,14 @@ router.get('/listgoods', function (req, res) {
       type: dbStore.sequelize.QueryTypes.SELECT
     })
   ]).spread(function (sqlItems) {
-    res.set('Content-Type',"application/json;charset=utf-8");
+    res.set('Content-Type', "application/json;charset=utf-8");
     let result = JSON.stringify({
       code: 0,
-      msg: sqlItems
+      msg : sqlItems
     });
     res.end(result);
   }).catch(function (err) {
-    res.set('Content-Type',"application/json;charset=utf-8");
+    res.set('Content-Type', "application/json;charset=utf-8");
     console.log(err);
     res.end(JSON.stringify({
       code: 101,
@@ -42,24 +43,43 @@ router.get('/listgoods', function (req, res) {
 });
 
 router.delete('/listgoods', function (req, res) {
-  const id    = req.query.id;
-  let tblName = 'tbl_store';
-  let sqlData = `DELETE FROM ${tblName} WHERE id=$id`;
+  const id     = req.query.id;
+  let tblName  = 'tbl_store';
+  let sqlQuery = `SELECT * FROM ${tblName} WHERE id=$id`;
+  let sqlData  = `DELETE FROM ${tblName} WHERE id=$id`;
   BBPromise.resolve([
+    dbStore.sequelize.query(sqlQuery, {
+      type: dbStore.sequelize.QueryTypes.SELECT,
+      bind: {
+        id: id
+      }
+    }),
     dbStore.sequelize.query(sqlData, {
       type: dbStore.sequelize.QueryTypes.DELETE,
       bind: {
         id: id
       }
     })
-  ]).spread(function () {
-    res.set('Content-Type',"application/json;charset=utf-8");
+  ]).spread(function (item) {
+    item = item[0];
+    // 删除文件
+    if(item.pic1){
+      fs.unlink(`public${item.pic1}`);  //images/goodspic/100114776391685174.jpg'
+    }
+    if(item.pic2){
+      fs.unlink(`public${item.pic2}`);
+    }
+    if(item.pic3){
+      fs.unlink(`public${item.pic3}`);
+    }
+
+    res.set('Content-Type', "application/json;charset=utf-8");
     let result = JSON.stringify({
       code: 0
     });
     res.end(result);
   }).catch(function (err) {
-    res.set('Content-Type',"application/json;charset=utf-8");
+    res.set('Content-Type', "application/json;charset=utf-8");
     let result = JSON.stringify({
       code: 101,
       msg : `ERROR: ${err}`
@@ -70,11 +90,11 @@ router.delete('/listgoods', function (req, res) {
 });
 
 router.post('/', function (req, res) {
-  res.set('Content-Type',"application/json;charset=utf-8");
+  res.set('Content-Type', "application/json;charset=utf-8");
   console.log(req.body);
   let result = JSON.stringify({
     code: 0,
-    msg: '服务器处理成功'
+    msg : '服务器处理成功'
   });
   res.end(result);
 });
